51da67c7d96323a3da58b040539d54e5f811dec9
rafa1326 <phillyaryeh@gmail.com>
2022-05-16 15:01:40 -0400

    when function

--- app.js
+++ app.js
@@ -32,11 +32,25 @@ const promptUser = () => {
             }
           }
     },
-      {
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+    
+    
+    {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
-       
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+          }
     }
    
      ]);
@@ -101,6 +115,10 @@ const promptUser = () => {
                   return false;
                 }
               }
+
+
+
+
         },
           {
             type: 'confirm',
