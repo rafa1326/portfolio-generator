5173680202c2f528869b6ad759c89327cd4ae51f
rafa1326 <phillyaryeh@gmail.com>
2022-05-18 14:10:17 -0400

    using asynchronous code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
- const fs = require('fs');
+ const { writeFile, copyFile } = require('./utils/generate-site.js');
  const inquirer = require('inquirer');
  const generatePage = require('./src/page-template');
  
@@ -128,23 +128,21 @@
  };
  
  promptUser()
-   .then(promptProject)
-   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData); 
-   
-     fs.writeFile('./dist/index.html', pageHTML, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Page created! Check out index.html in this directory to see it!');
+ .then(promptProject)
+ .then(portfolioData => {
+   return generatePage(portfolioData);
+ })
+ .then(pageHTML => {
+   return writeFile(pageHTML);
+ })
+ .then(writeFileResponse => {
+   console.log(writeFileResponse);
+   return copyFile();
+ })
+ .then(copyFileResponse => {
+   console.log(copyFileResponse);
+ })
+ .catch(err => {
+   console.log(err);
+ });
     
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log('Style sheet copied successfully!');
-      });
-    });
-   })
